ecbbc8ef531b9fc1c4399eb222ce79bf396f9bb0
Porwal-Hemant <155557012+Porwal-Hemant@users.noreply.github.com>
2025-02-28 23:01:37 +0530

    Add files via upload

--- ques1.cpp
+++ ques1.cpp
@@ -1,52 +1,96 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+// #define endl '\n'
+// #define debug(x) cout << #x << " = " << x << endl
+
+// void solve() 
+// {
+
+//     int n ; 
+
+//     cin >> n   ;  
+
+//     int count = 1 ;    // zero ko hamesha include karenge 
+
+//     int fullCycles = n / 15  ;  
+
+//     int rem  =  n % 15  ; 
+
+//     count = count + ( 3 * fullCycles ) ; 
+
+//     int startN  =  ( n  / 15 ) * 15  + 1  ; 
+
+//     for( int i = startN ;  i <= n ;  i++ ) 
+//     {
+//         if( i % 3 == i % 5 ) 
+//         {
+//             count++ ; 
+//         }
+//     }
+
+//     cout << count << endl  ; 
+
+// }
+
+// int32_t main() 
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+// #ifndef ONLINE_JUDGE
+//     freopen("D:/CodeForces/input", "r", stdin);
+//     freopen("D:/CodeForces/output", "w", stdout);
+// #endif
+
+//     int t = 1;
+//     cin >> t;
+//     for (int i = 1; i <= t; ++i) {
+//         // cout << "Case #" << i << ": ";
+//         solve();
+//     }
+
+//     return 0;
+// }
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 #define endl '\n'
-#define debug(x) cout << #x << " = " << x << endl
 
-int calc( int n , int k ) 
-{
+void solve() {  
+    int n, x, k;  // Using int unless necessary
+    cin >> n >> x >> k;
 
-    if( n == 1 ) 
-    {
-        return 0 ; 
+    string s;  
+    cin >> s;  
+
+    int startPoint = x;  
+    int count = 0;  // Tracks seconds
+    int ans = 0;  
+    size_t indexS = 0;  // Best practice for string index
+
+    while (indexS < s.length() && count < k) {
+        if (s[indexS] == 'L')  
+            startPoint--;  
+        else if (s[indexS] == 'R')  
+            startPoint++;  
+
+        if (startPoint == 0) {  
+            ans++;  
+            indexS = 0;  // Restart from the beginning
+        } else {  
+            indexS++;  
+        }
+
+        count++;  
     }
 
-    if( n <= k ) 
-    {
-        return 1  ; 
-    }
-
-    return 1  + calc( n - ( k - 1 ) ,  k ) ; 
-
-}
-
-void solve() 
-{
-
-    int n , k ; 
-
-    cin >> n >> k  ;
-
-    int f = k - 1  ;
-
-    // if( n == 1 )   
-    // {
-
-    //     cout <<  0  << endl ; 
-
-    // } 
-
-    // if( n <= k   ) 
-    // {
-
-    //     cout << 1 << endl ;  
-
-    // }
-
-    // cout << calc( n , f ) << endl ; 
-    cout << calc( n , k ) << endl  ;  
+    cout << ans << endl;  
 }
 
 int32_t main() {
@@ -58,13 +102,11 @@ int32_t main() {
     freopen("D:/CodeForces/output", "w", stdout);
 #endif
 
-    int t = 1;
+    int t;
     cin >> t;
-    for (int i = 1; i <= t; ++i) {
-        // cout << "Case #" << i << ": ";
+    while (t--) {
         solve();
     }
 
     return 0;
 }
-
--- ques2.cpp
+++ ques2.cpp
@@ -4,55 +4,219 @@ using namespace std;
 #define ll long long
 #define endl '\n'
 #define debug(x) cout << #x << " = " << x << endl
+// #define int long long 
 
-void solve()
-{   
-    int n ;  
-    cin >> n ; 
-    string s;
-    cin >> s;                   // Read the input string
-    int count_ones = 0;         // To count the number of ones
-    int count_consec_zeros = 0; // To count pairs of consecutive zeros
-    int current_zeros = 0;      // To track current sequence of zeros
+void solve() 
+{  
 
-    for (char c : s)
+    long long int n , x  , k ; 
+    cin >> n >> x >> k;
+
+    string s  ;  
+
+    cin >> s  ;  
+
+    long long int startPoint = x ;  
+
+    long long int count  = 0 ;   // count is keeping track of the seconds  
+
+    long long int ans  =  0 ;  
+
+    int indexS = 0; 
+
+    // LRRLL
+    while( indexS < s.length() && count < k   ) 
     {
-        if (c == '1')
-        {
-            count_ones++; // Increment count for '1'
-            if (current_zeros > 0)
-            {
-                count_consec_zeros++; // Increment count for consecutive zeros
-                current_zeros = 0;    // Reset current zero count
-            }
-        }
-        else if (c == '0')
-        {
-            current_zeros++; // Increment current zero count
-        }
+         if( s[indexS] == 'L' )  
+         {
+            startPoint = startPoint - 1  ;
+         }
+
+         else if( s[indexS] == 'R' ) 
+         {
+            startPoint = startPoint + 1   ; 
+         }
+
+         if( startPoint == 0 ) 
+         {
+            ans++ ;  
+            indexS = 0 ;  
+         }
+         else 
+         {
+            indexS++ ;  
+         }
+
+         count++  ;    // count toh hamesha hee ageh barega 
     }
 
-    // Check if there were trailing zeros at the end of the string
-    if (current_zeros > 0)
-    {
-        count_consec_zeros++;
-    }
+    cout << ans << endl ;  
 
-    // Output based on the comparison of counts
-    // cout << "cons zeros -> " << count_consec_zeros << endl;
-    // cout << "cons ones ->" << count_ones << endl;
-    if (count_consec_zeros >= count_ones)
-    {
-        cout << "no" << endl;
-    }
-    else
-    {
-        cout << "Yes" << endl;
-    }
 }
 
-int32_t main()
-{
+// void solve2() 
+// { 
+
+//     long long int n , x  , k ; 
+//     cin >> n >> x >> k;
+
+//     string s  ;  
+
+//     cin >> s  ;  
+
+//     long long int startPoint = x ;  
+
+//     long long int count  = 0 ;   // count is keeping track of the seconds  
+
+//     int indexS = 0;      
+
+//     bool checker =  false  ;   // jab phele par zero ho jayga tab mai isko false kar dunga 
+    
+//     while( indexS < s.length() && count < k   ) 
+//     {
+//          if( s[indexS] == 'L' )  
+//          {
+//             startPoint = startPoint - 1  ;
+//          }
+
+//          else if( s[indexS] == 'R' ) 
+//          {
+//             startPoint = startPoint + 1   ; 
+//          }
+
+//          if( startPoint == 0 ) 
+//          { 
+//             indexS = 0 ;  
+//             checker =  true  ;   
+//             break ; 
+//          }
+//          else 
+//          {
+//             indexS++ ;  
+//          }
+
+//          count++  ;    // count toh barao jab tak phele bar zero nahi ho jaata 
+//     }    
+
+//     // checker = false iska matlab voh zero huya hee nahi hai 
+
+//     if( checker == false  ) 
+//     {
+//         cout <<  0  << endl ; 
+//         return  ; 
+//     }
+
+//     int netk =  k - count   ; 
+
+//     // let us find the net steps from here 
+
+//     long long int left  = 0   ; 
+//     long long int right = 0   ;
+//     long long int factor  = 0  ;  
+
+//     for( int i = 0 ;  i < n  ;  i++ ) 
+//     {
+//         if( s[i] == 'L') 
+//         {
+//             left =  left + 1  ; 
+//         }
+//         else
+//         {
+//             right  = right + 1 ; 
+//         }
+
+//         if( left == right ) 
+//         {
+//             factor = i + 1  ; 
+//         }
+//     }
+
+//     long long int ans = 1  ; // ek bar toh ho chuka hai 
+
+//     ans += (netk / factor);
+   
+//     cout << ans << endl ;  
+
+// }
+
+void solve2() 
+{ 
+    long long n, x, k; 
+    cin >> n >> x >> k;
+
+    string s;  
+    cin >> s;  
+
+    long long startPoint = x;  
+    long long count = 0;   
+    long long ans = 0;  
+
+    int indexS = 0;      
+    bool checker = false; // Tracks if the robot has reached 0 at least once
+
+    // First, simulate the movement up to k seconds
+    while (indexS < s.length() && count < k) 
+    {
+        if (s[indexS] == 'L')  
+            startPoint--;
+        else if (s[indexS] == 'R') 
+            startPoint++;
+
+        count++ ;  // ek step ptoh bara na uppar vala     
+        if (startPoint == 0) 
+        {
+            ans++;  
+            indexS = 0;  
+            checker = true;   
+            break; 
+        }
+        else 
+        {
+            indexS++;  
+        } 
+    } 
+   
+    // cout << "ANS AFTER FIRST FIND ->" << ans << endl   ;  
+
+    // If zero is never reached, return 0
+    if (!checker) 
+    {
+        cout << 0 << endl; 
+        return;  
+    }
+
+    long long netk = k - count; // Remaining time after reaching 0 once
+    // cout << "NetK ->" << netk << endl ; 
+
+    // Calculate cycle length
+    long long left = 0, right = 0, factor = 0;  
+
+    for (int i = 0; i < n; i++) 
+    { 
+
+        if (s[i] == 'L') 
+            left++; 
+        else
+            right++;
+
+        if (left == right) 
+        {
+            factor = i + 1; 
+            break ; 
+        }
+
+    }
+    // cout << "factor->" << endl ; 
+    // If a full cycle exists, add the number of full cycles within netk
+    if (factor > 0) 
+    {
+        ans += ( netk / factor) ;  
+    }
+
+    cout << ans << endl;  
+}
+
+int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -63,11 +227,14 @@ int32_t main()
 
     int t = 1;
     cin >> t;
-    for (int i = 1; i <= t; ++i)
+    for (int i = 1; i <= t; ++i) 
     {
         // cout << "Case #" << i << ": ";
-        solve();
+        solve2();
     }
 
     return 0;
 }
+
+
+
