1f2a0e270352f61959a8703091d82b2c389cbe80
Porwal-Hemant <155557012+Porwal-Hemant@users.noreply.github.com>
2025-06-06 21:06:02 +0530

    Add files via upload

--- ques2.cpp
+++ ques2.cpp
@@ -1,61 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
 #define ll long long
+#define int long long
 #define endl '\n'
 #define debug(x) cout << #x << " = " << x << endl
 
-vector<int> fib(10);  // global vector of size 10
+void solve() 
+{
+    int xo , n  ;  
 
-void precomputeFib() {
-    fib[0] = 1;  // f1
-    fib[1] = 2;  // f2
+    cin >> xo >> n; 
 
-    for (int i = 2; i < 10; ++i) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-}
+    // haar 4 positions ke baad back to xo position aana hain 
 
-void solve() {
-    int n, m;  
-    cin >> n >> m;
+    int first  ;   
 
-    int biggerSide = fib[n - 1];  
-    int smallerSide = fib[n - 2];
+    int second  ;  
 
-    vector<vector<int>> boxes(m, vector<int>(3));  // storing all boxes as {width, length, height}
+    int third   ;  
 
-    for (int i = 0; i < m; i++) {
-        cin >> boxes[i][0] >> boxes[i][1] >> boxes[i][2];  // width, length, height
-    }
 
-    string ans = "";
+    if( xo % 2  == 0  ) 
+    {
 
-    for (int i = 0; i < m; i++) {
-        int width = boxes[i][0];
-        int length = boxes[i][1];
-        int height = boxes[i][2];
+        first = xo - 1  ;  
+        second = xo + 1 ;
 
-        if (height < biggerSide || min(width, length) < biggerSide) {
-            ans += '0';
-            continue;
-        }
+        third = second + 3  ;  
 
-        if (max(width, length) >= (biggerSide + smallerSide)) {
-            ans += '1';
-            continue;
-        }
-
-        if (height >= (biggerSide + smallerSide)) {
-            ans += '1';
-            continue;
-        }
-
-        ans += '0';  // none of the conditions satisfied
     }
 
-    cout << ans << endl;
+    else 
+    {
+        first = xo + 1 ;  
+        second = xo - 1 ;  
+
+        third = second - 3 ;  
+    } 
+
+    if( n % 4 == 0 ) 
+    {
+
+        cout << xo << endl  ;
+        return  ;  
+
+    }
+
+    if( n % 4 == 2 ) 
+    {
+
+        cout << second << endl  ; 
+        return  ;   
+
+    }
+
+    if( n % 4 == 1 ) 
+    {
+
+        if(  xo % 2 == 0 ) 
+        {   
+
+            int factor = n / 4 ;
+            cout << first - factor * 4 << endl ;  
+            return ;  
+
+        }
+        else 
+        {   
+
+            int factor = n / 4 ;
+            cout << first + factor * 4 << endl ;  
+            return ;  
+
+        }
+
+    }
+
+    if( n % 4 == 3 ) 
+    {
+
+        if( xo % 2 == 0 ) 
+        {   
+
+            int factor = n / 4 ;
+            cout << third + factor * 4 << endl ;  
+            return ;  
+
+        }
+        else 
+        {   
+
+            int factor = n / 4 ;
+            cout << third - factor * 4 << endl ;  
+            return ;  
+
+        }
+
+    }
+    
+
 }
 
 int32_t main() {
@@ -67,14 +111,16 @@ int32_t main() {
     freopen("D:/CodeForces/output", "w", stdout);
 #endif
 
-    precomputeFib();  // initializing global fib vector
-
     int t = 1;
     cin >> t;
-    for (int i = 1; i <= t; ++i) {
+    for (int i = 1; i <= t; ++i) 
+    {
         // cout << "Case #" << i << ": ";
         solve();
     }
 
     return 0;
+
 }
+
+
